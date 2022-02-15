7191400acada5a61b65c8d6c3004a52bbe960481
RymaMer <e1951642@cmaisonneuve.qc.ca>
2022-02-15 15:58:50 -0500

    fix some bugs html, reqIPPE,resIPPe, HP

--- public/IPPE/reqIPPE.js
+++ public/IPPE/reqIPPE.js
@@ -10,4 +10,3 @@ function menuBareNaviguation(){
 document.getElementById('buttonMenuBareNaviguation').addEventListener("click",menuBareNaviguation);
 
 
-(function(){window['__CF$cv$params']={r:'6dd775758e267150',m:'j3lo4bQ1sGDiEPg3VtZr1hCF5uURnoppcF9ZjpdvPHw-1644853896-0-AXBN0ZjAFk6oi2MjtISztTrSIZAP0oGSO7ZWzVrN+xjXDOwBAImeOFC4n4ByOg/9azCCFRSmPhb2U4plqKDbTa3gnc0hBZLSHY9x7CFBpKc9IJLdVlv53/cF0NZlK+rCAbni11blP9K3SroOEKxH0ElHGPlIPeSA3jWbhuZADs7HHtbuDbMIAW0Ia6bqxdtFgW91bev6IMMW8sxwoEJGISo=',s:[0x855dd38c62,0xd7f9ed845b],}})();
