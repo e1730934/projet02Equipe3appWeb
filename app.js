b9df198e0b4550b112afc2c8fb6ef27ab0cf38d5
David Dechaine <e0771661@cmaisonneuve.qc.ca>
2022-02-14 19:59:46 -0500

    Changement app.js
    Static routing

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const app = express();
-//const path = require("path")
+const path = require("path")
 
-app.use(express.static(__dirname + "/appweb")); 
+app.use(express.static(path.join("public")));
 
 //passer d'index a la page de détail d'articles
 app.get("/",(req, res) =>{
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 app.get("/ProfesseurAccueil",(req, res) =>{
-    res.sendFile(__dirname + "/Teacher_Hp/TeacherHp.html");
+    res.sendFile(__dirname + "/public/Teacher_Hp/TeacherHp.html");
 });
 
 app.get("/EtudiantAccueil",(req, res) =>{
-    res.sendFile(__dirname + "/Teacher_Hp/Student.html");
+    res.sendFile(__dirname + "/public/Teacher_Hp/Student.html");
 });
 
 app.get("/IPPE",(req, res) =>{
-    res.sendFile(__dirname + "/IPPE/reqIPPE.html");
+    res.sendFile(__dirname + "/public/IPPE/reqIPPE.html");
 });
 
 app.listen(PORT, () => {
