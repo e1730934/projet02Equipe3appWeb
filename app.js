e2858d18810bea225482fbbe0c93f07efb5beae7
RymaMer <e1951642@cmaisonneuve.qc.ca>
2022-02-14 20:02:15 -0500

    essaie aide David, nest plus necaissaire

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const app = express();
-//const path = require("path")
+const path = require("path")
 
-app.use(express.static(__dirname + "/appweb")); 
+app.use(express.static("CLIENT")); 
 
 //passer d'index a la page de détail d'articles
 app.get("/",(req, res) =>{
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "CLIENT/index.html");
 });
 
 app.get("/ProfesseurAccueil",(req, res) =>{
@@ -19,7 +19,11 @@ app.get("/EtudiantAccueil",(req, res) =>{
 });
 
 app.get("/IPPE",(req, res) =>{
-    res.sendFile(__dirname + "/IPPE/reqIPPE.html");
+    res.sendFile(__dirname + "/CLIENT/IPPE/reqIPPE.html");
+});
+
+app.get("/Accueil",(req, res) =>{
+    res.sendFile(__dirname + "/CLIENT/accueil/accueil.html");
 });
 
 app.listen(PORT, () => {
