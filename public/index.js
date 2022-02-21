36aa9a4caae7f97b6247d2123e0aba8fb95000a1
Dan Bagalwa <e2048889@cmaisonneuve.qc.ca>
2022-02-21 12:08:18 -0500

    Login
    Mise à jour de fetch et création token

--- public/index.js
+++ public/index.js
@@ -1,16 +1,50 @@
-function etudiant() {
-    let divEtudiant = document.getElementById(id01)
-    let divEnseignant = document.getElementById(id02)
-    // divEnseignant.style.display = "";
-    document.getElementById("test1").innerHTML = "Identification étudiant"
-    // divEtudiant.style.display = "block";
-    console.log(divEtudiant)
-  }
-  function enseignant() {
-    let divEtudiant = document.getElementById(id01)
-    let divEnseignant = document.getElementById(id02)
-    // divEnseignant.style.display = "";
-    document.getElementById("test1").innerHTML = "Identification enseignant"
-    // divEtudiant.style.display = "none";
 
-  }
+
+window.onload =  function () {
+
+    const BtnEt = document.getElementById('BtnEtudiant');
+    const BtnEn = document.getElementById('BtnEnseignant');
+    const c = document.getElementById('connection');
+
+
+    BtnEt.addEventListener('click',()=>{document.getElementById("id").innerText = "Identification étudiant";})
+    BtnEn.addEventListener('click',()=>{document.getElementById("id").innerText = "Identification enseignant";})
+
+    c.addEventListener('click', async () => {
+
+
+
+        const utilisateur = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        const connexion = { username: utilisateur, password: password };
+        const response = await fetch('http://localhost:5000/login', {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'POST',
+            body: connexion,
+
+        });
+
+
+        if (response.success) {
+            await response.json();
+            if (response.Etudiant ){
+                sessionStorage.setItem('token', etudiant)
+            
+            }
+            else{
+                sessionStorage.setItem('token', enseignat)
+            } 
+            window.location.href = "./StudentHp.html";
+        }
+
+        // Redirige vers la page pour l'enseignant
+
+        // Redirige vers la page pour l'étudiant
+
+    });
+
+}
+
+
+
