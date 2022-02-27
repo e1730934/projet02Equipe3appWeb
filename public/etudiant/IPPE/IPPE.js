7a3106e55e5438284de714b9e53213a9a811e6fe
e1730934 <e1730934@cmaisonneuve.qc.ca>
2022-02-27 15:29:35 -0500

    IPPErequest.html terminée

--- public/etudiant/IPPE/IPPE.js
+++ public/etudiant/IPPE/IPPE.js
@@ -1,33 +1,15 @@
 function Soumettre(event) {
   event.preventDefault();
-  const test = new FormData(event.target);
-  console.log(test.get('NomFamille'));
+  const formData = new URLSearchParams(new FormData(event.target));
 
-  const NomFamille = document.getElementById('NomFamille').value;
-  const Prenom1 = document.getElementById('Prenom1').value;
-  const Prenom2 = document.getElementById('Prenom2').value;
-  const Sexe = document.getElementById('Sexe').value;
-  const DateNaissance = document.getElementById('DateNaissance').value;
-
-  const formData = new FormData();
-  formData.append('NomFamille', NomFamille);
-  formData.append('Prenom1', Prenom1);
-  formData.append('Prenom2', Prenom2);
-  formData.append('Sexe', Sexe);
-  formData.append('DateNaissance', DateNaissance);
-
-  const myHeaders = new Headers({
-    'Content-Type': 'application/x-www-form-urlencoded',
-  });
   fetch('http://localhost:5000/rechercher/infos/personnes', {
     method: 'POST',
     body: formData,
-    // headers: myHeaders,
   })
     .then((res) => res.json())
     .then((resJson) => {
       localStorage.setItem('infoIPPE', JSON.stringify(resJson.data));
-      // window.location.href = 'http://localhost:3000/etudiant/IPPE/resultat';
+      window.location.href = 'http://localhost:3000/etudiant/IPPE/resultat';
     })
     .catch((err) => {
       alert(`Erreur: ${err}`);
