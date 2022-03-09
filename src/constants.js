7b836d94114e65849c00c79ba3f254834de98a16
Marc <mlevasseur@cmaisonneuve.qc.ca>
2022-03-09 14:39:52 -0500

    Noyau Vue.js

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+const svrURL = 'https://pokemonsapi.herokuapp.com';
+
+// eslint-disable-next-line import/prefer-default-export
+export { svrURL };
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 class="title is-1 has-text-centered">Hello World</h1>
+</template>
+
+<script>
+
+// noinspection JSUnusedGlobalSymbols
+export default {
+    name: 'HomeView',
+};
+</script>
+
+<style scoped>
+</style>
