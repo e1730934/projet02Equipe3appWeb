a486ceda6ff7909b937ea2c176931252c913afeb
Karim Amzert <e2069838@cmaisonneuve.qc.ca>
2022-03-23 20:51:34 +0000

    Upload New File

---
+++ IBAF.js
@@ -0,0 +1,17 @@
+function EnvoiInfo(event){
+    event.preventDefault();
+    let formData = new URLSearchParams(new FormData(event.target));
+
+    fetch('http://localhost:3000/enseignant/reponse/IBAF',{method:"POST", body:formData})
+        .then(res =>{
+            return res.json();
+        })
+        .then(resJson =>{
+            if(resJson.success){
+                res.status(200).json({ success :"Information Modifiée avec succés"});
+            }
+        })
+        .catch(err =>{
+            alert(`Erreur: ${err}`)
+        })
+}
