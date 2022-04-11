98462f05218bbab673bb8174716fe881e0a09101
e1920614 <e1920614@cmaisonneuve.qc.ca>
2022-04-11 11:26:48 -0300

    routes vue.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
+import ObjetView from '@/views/ObjetView.vue';
+import ArmeView from '@/views/ArmeView.vue';
+import ValeurView from '@/views/ValeurView.vue';
+import EtudiantView from '@/views/EtudiantView.vue';
+
 
 const routes = [
     {
         path: '/',
         name: 'home',
         component: HomeView,
+    },
+    {
+        path: '/objet/idObjet',
+        name: 'objet',
+        component: ObjetView,
+    },
+    {
+        path: '/arme/:idArme',
+        name: 'arme',
+        component: ArmeView,
+    },
+    {
+        path: '/valeur/:idValeur',
+        name: 'valeur',
+        component: ValeurView,
+    },
+    {
+        path: '/etudiant',
+        name: 'etudiant',
+        component: EtudiantView,
     },
 ];
 
