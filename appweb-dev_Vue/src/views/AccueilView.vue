5851c9b2c0d9d1bc46818154936e86c4865c2f68
RymaMer <e1951642@cmaisonneuve.qc.ca>
2022-04-18 21:16:25 -0400

    add vue

---
+++ appweb-dev_Vue/src/views/AccueilView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1 class="title is-1 has-text-centered">CRTP</h1>
+    </div>
+</template>
+
+<script>
+
+// noinspection JSUnusedGlobalSymbols
+export default {
+    name: 'AccueilView',
+};
+</script>
+
+<style scoped>
+</style>
