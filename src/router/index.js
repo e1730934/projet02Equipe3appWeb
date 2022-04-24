4a5f6d87ccc3bcb42b0b17a902d26d006119aa7f
Karim Amzert <e2069838@cmaisonneuve.qc.ca>
2022-04-24 02:46:37 +0000

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import RequeteIPPEView from '@/views/RequeteIPPEView.vue';
 import PersonnesView from '@/views/PersonnesView.vue';
 import ConditionView from '@/views/ConditionView.vue';
 import PersonneView from '@/views/PersonneView.vue';
-import ObjetView from '@/views/ObjetsView.vue';
+import ObjetView from '@/views/ObjetView.vue';
 import ArmeView from '@/views/ArmeView.vue';
 import ReponseIPPEView from '@/views/ReponseIPPEView.vue';
 import ValeurView from '@/views/ValeurView.vue';
@@ -13,6 +13,7 @@ import EtudiantView from '@/views/EtudiantView.vue';
 import SAAQView from '@/views/SAAQView.vue';
 import ArmesView from '@/views/ArmesView.vue';
 import ValeursView from '@/views/ValeursView.vue';
+import ObjetsView from '@/views/ObjetsView.vue';
 
 const routes = [
     {
@@ -82,6 +83,11 @@ const routes = [
         path: '/valeurs',
         name: 'valeursView',
         component: ValeursView,
+    },
+    {
+        path: '/objets',
+        name: 'objetsView',
+        component: ObjetsView,
     },
     {
         path: '/SAAQ',
