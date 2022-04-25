958422f0105c87efa4b9b180b9397ed1a3b1d397
Bilal Khendaf <e1730934@cmaisonneuve.qc.ca>
2022-04-25 09:08:35 -0400

    Fix ESLINT

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <MenuProfesseurView v-if="Professeur == true"/>
+        <MenuProfesseurView v-if="Professeur === true"/>
         <HeaderView/>
         <router-view/>
     </div>
